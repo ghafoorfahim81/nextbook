d64ba3db45296aabc9328292d23010ade33e62cf
Ghafoorfahim <fahimghafoor@gmail.com>
2025-08-10 16:17:55 +0430

    add stock models and migration

---
+++ app/Models/Inventory/Stock.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models\Inventory;
+
+use App\Models\Administration\Store;
+use App\Models\Administration\UnitMeasure;
+use Illuminate\Database\Eloquent\Model;
+
+class Stock extends Model
+{
+    protected $table = 'stocks';
+    protected $primaryKey = 'id';
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    public function item()
+    {
+        return $this->belongsTo(Item::class);
+    }
+
+    public function branch()
+    {
+        return $this->belongsTo(\App\Models\Administration\Branch::class);
+    }
+
+    public function unit_measure()
+    {
+        return $this->belongsTo(UnitMeasure::class);
+    }
+
+    public function purchase()
+    {
+        return $this->belongsTo(Purchase::class);
+    }
+
+    public function store()
+    {
+        return $this->belongsTo(Store::class);
+    }
+}
---
+++ database/migrations/2025_08_10_113115_create_stocks_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->char('id', 26)->primary();
+            $table->char('item_id');
+            $table->char('store_id',26);
+            $table->char('unit_measure_id',26);
+            $table->double('quantity');
+            $table->double('cost')->unsigned();
+            $table->double('free')->nullable();
+            $table->string('batch')->nullable();
+            $table->double('discount')->nullable();
+            $table->double('tax')->nullable();
+            $table->date('date')->nullable();
+            $table->date('expire_date')->nullable();
+            $table->char('purchase_id',26)->nullable();
+            $table->timestamps();
+        });
+
+        schema::table('stocks', function (Blueprint $table) {
+            $table->foreign('item_id')->references('id')->on('items');
+            $table->foreign('store_id')->references('id')->on('stores');
+            $table->foreign('unit_measure_id')->references('id')->on('unit_measures');
+//            $table->foreign('purchase_id')->references('id')->on('purchases');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stocks');
+    }
+};
