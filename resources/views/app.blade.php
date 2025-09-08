99ecd4fc54477af98d64de36265e3cae92a7c4f3
Ghafoorfahim <82256682+ghafoorfahim81@users.noreply.github.com>
2025-09-08 21:39:48 +0430

    Refactor(app.blade.php): vite import changed

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
-    @vite(['resources/js/app.js', 'resources/css/app.css'])
+    @vite('resources/js/app.js')
+
+
     @inertiaHead
     @routes
 </head>
