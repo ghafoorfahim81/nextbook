47ad22117f896479f7169af35a4c03f5d40cba46
Ghafoorfahim <82256682+ghafoorfahim81@users.noreply.github.com>
2025-10-10 20:33:49 +0430

    fix: some bugs fixed

--- resources/js/Pages/Purchase/Purchases/Create.vue
+++ resources/js/Pages/Purchase/Purchases/Create.vue
@@ -494,7 +494,7 @@ const addRow = () => {
             </div>
             <div class="rounded-xl border bg-card shadow-sm overflow-x-auto max-h-72">
                 <table class="w-full table-fixed min-w-[1000px] purchase-table border-separate">
-                    <thead class="bg-card sticky top-0 z-[200]">
+                    <thead class="bg-card sticky top-0" :class="form.selected_sale_purchase_type === 'cash' ? 'z-[200]' : ''">
                         <tr class="rounded-xltext-muted-foreground font-semibold text-sm text-violet-500">
                             <th class="px-1 py-1 w-5 min-w-5">#</th>
                             <th class="px-1 py-1 w-40 min-w-64">{{ t('item.item') }}</th>
