5fa678c757aac4dfad5bcdc374e11d23fb436778
Ghafoorfahim <82256682+ghafoorfahim81@users.noreply.github.com>
2025-11-13 21:03:36 +0430

    feat: add cash customer in ledger seeder

--- database/seeders/Ledger/LedgerSeeder.php
+++ database/seeders/Ledger/LedgerSeeder.php
@@ -3,8 +3,11 @@
 namespace Database\Seeders\Ledger;
 
 use App\Models\Ledger\Ledger;
+use App\Models\User;
+use App\Models\Administration\Branch;
+use App\Models\Administration\Currency;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Str;
 class LedgerSeeder extends Seeder
 {
     /**
@@ -12,6 +15,16 @@ class LedgerSeeder extends Seeder
      */
     public function run(): void
     {
-        Ledger::factory()->count(5)->create();
+        // Ledger::factory()->count(5)->create();
+        $ledgers = [
+            [
+                'id' => Str::ulid(),
+                'name' => 'Cash customer',
+                'code' => 'CASH-CUST',
+                'type' => 'customer',
+                'created_by' => User::where('email', 'admin@nextbook.com')->first()->id,
+            ],
+        ];
+        Ledger::insert($ledgers);
     }
 }
